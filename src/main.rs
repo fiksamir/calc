fe4e355760c1ab26c003edf7e7f95d06c7e6b3af
Denys <fiksamir@gmail.com>
2023-05-25 23:12:27 +0300

    add calc

---
+++ src/main.rs
@@ -0,0 +1,58 @@
+
+use std::io;
+
+fn main() {
+    println!("Please input first number.");
+
+    let mut first_number = String::new();
+    
+    loop{ 
+        io::stdin()
+            .read_line(&mut first_number)
+            .expect("Failed to read line");
+
+        let first_number: f32 = match first_number.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("Please input second number.");
+
+        let mut second_number = String::new();
+
+        io::stdin()
+            .read_line(&mut second_number)
+            .expect("Failed to read line");
+
+        let second_number: f32 = match second_number.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("Please chose action: 1:\"+\", 2:\"-\", 3:\"/\", 4:\"*\""); 
+
+        let mut action = String::new();
+
+        io::stdin()
+            .read_line(&mut action)
+            .expect("Failed to read line");   
+
+        let action: u8 = match action.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match action {
+            1 => println!("{:?} + {:?} = {:?}",first_number, second_number, first_number + second_number),
+            2 => println!("{:?} - {:?} = {:?}",first_number, second_number, first_number - second_number),
+            3 => println!("{:?} / {:?} = {:?}",first_number, second_number, first_number / second_number),
+            4 => println!("{:?} * {:?} = {:?}",first_number, second_number, first_number * second_number),
+            _ => {
+                println!("Wrong action!");
+                break;
+            }
+        }
+
+        break;
+    }
+    
+}
